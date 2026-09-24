195bda983dc451829a152f411a6abf64a8db1059
josecojr <josecojr@users.noreply.github.com>
2026-09-24 13:42:34 -0300

    feat: vistos com selo, progresso e continuar de onde parou + v20 por cima

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-self.addEventListener('install',function(e){self.skipWaiting();});self.addEventListener('fetch',function(e){});
+var JRV='jrvideo-v20';
+self.addEventListener('install',function(e){self.skipWaiting();});
+self.addEventListener('activate',function(e){
+ e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){});
